7c82e2fd266508c2ad3c21be3c7af14b04f4d307
Ryan-T-Bell <ryan.t.bell92@gmail.com>
2023-04-11 12:05:38 -0400

    forge parser made

--- cli/input_parser.c
+++ cli/input_parser.c
@@ -49,62 +49,113 @@ int isUse(char* input) {
 //////////////////////////////////////////////////////////////////////////////////////////
 // C2: FORGE
 //////////////////////////////////////////////////////////////////////////////////////////
-char* getFirstWord(char* input) {
-    char firstWord[20];
-    int i = 0;
-
-    // Extract the first word from the input string
-    while (input[i] != '\0' && input[i] != ' ' && i < 20) {
-        firstWord[i] = input[i];
-        i++;
-    }
-    
-    // Null-terminate the first word string
-    firstWord[i] = '\0';
-    return firstWord;
-}
 
 int isForge(char* input) {
-    if (strcmp(getFirstWord(input), "forge") == 0)
-        return 1;
-    
-    return 0;
+    return input[0] == 'f' && input[1] == 'o' && input[2] == 'r' && input[3] == 'g' && input[4] == 'e';
 }
 
-int parseForgeInput(char* input) {
-    char *word;
-    
-    // First call to strtok with input string
-    word = strtok(input, " ");
+int getNextIndexOf(int index, char c, char* input) {
+    int i = index;
+    while (i < strlen(input)) {
+        if (input[i] == c)
+            return i;
+        i++;
+    }
+    return i;
+}
 
-    while (word != NULL) {
-        printf("%s\n", word);
-        word = strtok(NULL, " ");
+char* getNextWord(int index, char* input) {
+
+    int i = index;
+    int len = strlen(input);
+    char c;
+    char* str;
+
+    // Null check 1
+    if (index == -1)
+        return NULL;
+    
+    str = (char*)malloc(len - i + 1); // Allocate memory
+
+    // Null check 2
+    if (str == NULL) {
+        printf("Error: Failed to allocate memory.\n");
+        return NULL;
     }
 
-    return 1;
+    int j = 0;
+
+    // Build string
+    while (i < len && input[i] != ' ' && input[i] != '\0' && input[i] != '\n') {
+        str[j++] = input[i++];
+    }
+
+    // Null-terminate the new string
+    str[j] = '\0';
+
+    return str;
 }
+
+// Parse forge command line and return normalized string for compiler
+char* parseForgeInput(char* input) {
+    int i = 5;
+    char *flag, *arg, *a, *f, *os, *arch, *ip, *port;
+
+    while (i < strlen(input) && input[i]) {
+        // Get next flag
+        i = getNextIndexOf(i, '-', input);
+        flag = getNextWord(i, input);
+
+        // Get next argument
+        i = getNextIndexOf(i, ' ', input) + 1;
+        arg = getNextWord(i, input);
+
+        // Store argument
+        if (strcmp(flag, "-a") == 0)
+            a = arg;
+        else if (strcmp(flag, "-f") == 0)
+            f = arg;
+        else if (strcmp(flag, "-os") == 0)
+            os = arg;
+        else if (strcmp(flag, "-arch") == 0)
+            arch = arg;
+        else if (strcmp(flag, "-ip") == 0)
+            ip = arg;
+        else if (strcmp(flag, "-p") == 0 || strcmp(flag, "-port") == 0)
+            port = arg;
+        else if (strcmp(flag, "") == 0)
+            continue;
+        else
+            printf("Invalid flag: %s\n", flag);
+    }
+
+    printf("forge %s %s %s %s %s %s\n", a, f, os, arch, ip, port);
+    return "forged";
+}
+
+
+
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // C2
 //////////////////////////////////////////////////////////////////////////////////////////
 int parse_c2(char *input) {
-    if (isHelp(input)) {
+    if (isHelp(input)) {                    // Help
         printf("%s", msgForgeHelp);
     }
-    else if (isExit(input)) {
+    else if (isExit(input)) {               // Exit
         return confirmIfUserWantsToExit();
     }
-    else if (isLS(input)) {
+    else if (isLS(input)) {                 // LS
         printf("LS\n");                     // TODO Implement
     }
-    else if (isRM(input)) {
+    else if (isRM(input)) {                 // RM
         printf("rm\n");                     // TODO Implement
     }
-    else if (isForge(input)) {
-        parseForgeInput(input);
+    else if (isForge(input)) {              // Forge
+        parseForgeInput(input);             // TODO Implement
     }
-    else if (isUse(input)) {
+    else if (isUse(input)) {                // Use
         printf("Use\n");                    // TODO Implement
     }
     else {
