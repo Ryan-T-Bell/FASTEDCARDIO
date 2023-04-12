957611bb31961a5a85f8c1efcdcc23ad31d40343
Ryan-T-Bell <ryan.t.bell92@gmail.com>
2023-04-12 18:28:33 -0400

    merge

--- forge/compiler.c
+++ forge/compiler.c
@@ -5,5 +5,16 @@ int argumentsValid(char ** arguments) {
 }
 
 int compile(char ** arguments) {
-
+    char * agent = arguments[0];
+    char * format = arguments[1];
+    char * target = arguments[2];
+    char * ip = arguments[3];
+    char * port = arguments[4];
+    
+    char * command = malloc(1000);
+    sprintf(command, "gcc -target %s, -o %s %s.c -lssl -lcrypto", target, agent, agent);
+    system(command);
+    free(command);
+    
+    return 1;
 }
