1d7843b59c921f9182deecae29436af6bd6e3c46
Ryan-T-Bell <ryan.t.bell92@gmail.com>
2023-04-16 08:24:31 -0400

    validate arguments v1

--- src/forge/forge.c
+++ src/forge/forge.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
 #include "forge.h"
 #include "arg_parser.c"
 #include "compiler.c"
@@ -39,7 +40,6 @@ void set_argument(struct Forge* obj, char* flag, char* arg) {
         obj->ip = arg;
     else if (strcmp(flag, "-p") == 0 || strcmp(flag, "-port") == 0)
         obj->port = arg;
-    
 }
 
 void parse_arguments(struct Forge* obj, char* input) {
@@ -65,52 +65,71 @@ void parse_arguments(struct Forge* obj, char* input) {
     }
 }
 
-int is_agent(char* agent) {
-    if(strcmp(agent, "beacon") == 0 || strcmp(agent, "trigger") == 0 || strcmp(agent, "rat") == 0)  {
+int is_agent(struct Forge* obj) {
+    if(strcmp(obj->agent, "beacon") == 0 || strcmp(obj->agent, "trigger") == 0 || strcmp(obj->agent, "rat") == 0)  {
         return 1;
     } else {
-        printf("Invalid Flag | Agent (-a): %s\n  Acceptable values: beacon, trigger, or rat", agent);
+        printf("Invalid Flag | Agent (-a): %s\n  Acceptable values: beacon, trigger, or rat", obj->agent);
         return 0;
     }
 }
 
-int is_format(char* format) {
-    if(strcmp(format, "exe") == 0 || strcmp(format, "library") == 0)  {
+int is_format(struct Forge* obj) {
+    if(strcmp(obj->format, "exe") == 0 || strcmp(obj->format, "library") == 0)  {
         return 1;
     } else {
-        printf("Invalid Flag | Format (-f): %s\n  Acceptable values: exe or library", format);
+        printf("Invalid Flag | Format (-f): %s\n  Acceptable values: exe or library", obj->format);
         return 0;
     }
 }
 
-int is_target(char* target) {
-    if(strcmp(target, "windows") == 0 || strcmp(target, "linux") == 0)  {
+int is_target(struct Forge* obj) {
+    if(strcmp(obj->target, "windows") == 0 || strcmp(obj->target, "linux") == 0)  {
         return 1;
     } else {
-        printf("Invalid Flag | Target (-t): %s\n  Acceptable values: windows or linux", target);
+        printf("Invalid Flag | Target (-t): %s\n  Acceptable values: windows or linux", obj->target);
         return 0;
     }
 }
 
-int is_ip(char* format, char* ip) {
+int is_ip(struct Forge* obj) {
     
     // Agent is trigger, no return IP needed
-    if (strcmp(format, "trigger") == 0) {
+    if (strcmp(obj->format, "trigger") == 0) {
         return 1;
-    } else {
-        char *token = strtok(ip, ".");
     
-        while (token != NULL) {
-            printf("Token: %s\n", token);
-            token = strtok(NULL, ".");
+    // Agent is beacon or rat, check if IP
+    } else {
+        struct sockaddr_in sa4;
+        struct sockaddr_in6 sa6;
+
+        // Try to parse as IPv4 address
+        if (inet_pton(AF_INET, obj->ip, &(sa4.sin_addr)) == 1) {
+            return 1;
+        }
+
+        // Try to parse as IPv6 address
+        if (inet_pton(AF_INET6, obj->ip, &(sa6.sin6_addr)) == 1) {
+            return 1;
+        }
+
+        return 0;
     }
-    }
+}
+
+int is_port(struct Forge* obj) {
+
+    // Convert string to integer
+    int port_num = atoi(obj->port);
+
+    if (port_num >= 1 && port_num <= 65535)
+        return 1;
+
+    return 0;
 }
 
 int validate_arguments(struct Forge* obj) {
-    
-    
-    return 1;
+    return is_agent(obj) && is_format(obj) && is_target(obj) && is_ip(obj) && is_port(obj);
 }
 
 void compile(struct Forge* obj) {
